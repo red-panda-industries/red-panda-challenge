c14bd5e174bdc505211f180203c7fdd8b95481bf
Jack Willis <jwillis@sprocketsecurity.com>
2024-06-08 22:52:55 -0500

    Connect to Discord using bot token

--- app.rb
+++ app.rb
@@ -5,6 +5,23 @@ require 'sqlite3'
 
 logger = Logger.new($stdout)
 
-logger.info 'Hello, world!'
+logger.info 'Red Panda Challenge Bot is starting...'
 
-binding.pry
+bot = Discordrb::Commands::CommandBot.new(
+  token: ENV['DISCORD_BOT_TOKEN'],
+  intents: [:server_messages],
+  prefix: '!',
+)
+
+logger.info 'Red Panda Challenge is running.'
+logger.info "This bot's invite URL is #{bot.invite_url}"
+
+bot.message(content: 'Ping!') do |event|
+  event.respond 'Pong!'
+end
+
+bot.command(:user) do |event|
+  event.respond event.user.name
+end
+
+bot.run()
