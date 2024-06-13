de98287bae00b649b0290ec48dabd9cb298fb224
Jack Willis <jack@attac.us>
2024-06-12 22:38:31 -0500

    Add a !count command

--- app.rb
+++ app.rb
@@ -18,12 +18,23 @@ bot = Discordrb::Commands::CommandBot.new(
 logger.info 'Red Panda Challenge bot is running.'
 logger.info "This bot's invite URL is #{bot.invite_url}"
 
+################################################################
+
 bot.message(content: 'Ping!') do |event|
   event << 'Pong!'
 end
 
 bot.command('user') do |event|
-  event << "Your username is #{user.name}, and your ID is #{user.id}"
+  event << "Your username is #{event.user.name}, and your ID is #{event.user.id}"
+end
+
+@counts = {}
+
+bot.command('count') do |event|
+  user_id = event.user.id
+  @counts[user_id] ||= 0
+  @counts[user_id] += 1
+  event << "You have used this command #{@counts[user_id]} times now."
 end
 
 bot.run()
