934833c5cd94b9e86fe030f3b159ef20d8cbdf28
Jack Willis <jwillis@sprocketsecurity.com>
2024-06-23 09:07:18 -0500

    Add more info to `!michelle` command

--- app/bots/red_panda_challenge_bot.rb
+++ app/bots/red_panda_challenge_bot.rb
@@ -49,6 +49,7 @@ class RedPandaChallengeBot
     bot.command(command_name) do |event|
       logger.info "#{event.user.name} (#{event.user.id}): #{event.message.content}"
       __send__(handler_method_name, event)
+      nil # Return nil to prevent the command from echoing the return value
     end
   end
 
@@ -56,25 +57,37 @@ class RedPandaChallengeBot
 
   def handle_user_info!(event)
     user = User.from_discord_event(event)
+
     event << "Your username is #{user.username} and your Discord ID is #{user.discord_id}."
     event << "Your count is #{user.count}. Your created at is #{user.created_at}. Your updated at is #{user.updated_at}."
   end
 
   def handle_count!(event)
     user = User.from_discord_event(event)
+
     user.count += 1
     user.save!
+
     event << "You have used this command #{user.count} times now."
   end
 
   def handle_michelle!(event)
     user = User.from_discord_event(event)
+
     if user.has_completed_michelle_obama_challenge_today?
       event << 'You have already completed the Michelle Obama challenge today.'
     else
       user.complete_michelle_obama_challenge!
       event << 'You have completed the Michelle Obama challenge for today!'
     end
-    event << "You have completed the Michelle Obama challenge #{user.michelle_obama_challenge_entries.count} times."
+
+    challenge_entries = user.michelle_obama_challenge_entries
+
+    event << "You have completed the Michelle Obama challenge #{challenge_entries.size} times."
+    event << 'Here are the dates you have completed the challenge:'
+    challenge_entries.each do |entry|
+      event << "- #{entry.date}"
+    end
+    event << 'Keep up the good work! - Michelle Obama'
   end
 end
