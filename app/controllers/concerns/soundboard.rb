c612af8efaa4b2547ccb9a3694f4cee5fdab5c2c
Jack Willis <jwillis@sprocketsecurity.com>
2024-07-03 07:56:58 -0500

    Minor refactoring, add logs

--- app/controllers/concerns/soundboard.rb
+++ app/controllers/concerns/soundboard.rb
@@ -3,29 +3,36 @@ require 'timeout'
 module Soundboard
   VOICE_CHANNEL_TIMEOUT_SECONDS = 5
 
-  # Mutexes to synchronize playing sounds on a per-server basis
+  # Mutexes to be created for each server, to prevent multiple sounds from playing at once on the same server.
+  # This is necessary because the bot can only be connected to one voice channel at a time per server.
   @@server_mutexes = {}
 
   def play_wow_ethan_sound!
-    play_sound_synchronized!('wow_ethan.opus')
+    play_sound!('wow_ethan.opus')
   end
 
   private
 
-  def play_sound_synchronized!(file_name)
+  def play_sound!(file_name)
+    flush_message_buffer!
+
+    logger.info "Attempting to play sound #{file_name.inspect}"
+    logger.info 'Waiting for soundboard mutex'
     server_mutex.synchronize do
-      play_sound!(file_name)
+      logger.info 'Acquired soundboard mutex'
+      connect_to_voice_channel! or return
+      begin
+        play_sound_file!(file_name)
+      rescue StandardError => e
+        logger.error "Error during sound playback: #{e.message}"
+      ensure
+        leave_voice_channel!
+      end
     end
   end
 
-  def play_sound!(file_name)
+  def play_sound_file!(file_name)
     sound_path = File.join(::Application.sounds_directory, file_name).to_s
-    logger.info "Attempting to play sound #{sound_path.inspect}"
-
-    flush_message_buffer!
-
-    voice_channel = connect_to_voice_channel!
-    return unless voice_channel
 
     if file_name.end_with?('.dca')
       event.voice.play_dca(sound_path)
@@ -33,9 +40,7 @@ module Soundboard
       event.voice.play_file(sound_path)
     end
 
-    leave_voice_channel!
-
-    logger.info "\e[1m\e[32mPlayed sound #{sound_path.inspect} in voice channel #{voice_channel.name.inspect}\e[0m"
+    logger.info "\e[1m\e[32mPlayed sound #{sound_path.inspect} in voice channel #{event.user.voice_channel.name.inspect}\e[0m"
   end
 
   # returns the voice channel the bot is connected to,
@@ -45,7 +50,7 @@ module Soundboard
     channel = event.user.voice_channel
 
     if channel.nil?
-      logger.warn "User #{event.user.name.inspect} (#{event.user.id}) is not in a voice channel"
+      logger.warn 'User is not in a voice channel'
       return nil
     end
 
@@ -65,14 +70,17 @@ module Soundboard
 
   def leave_voice_channel!
     bot.voice_destroy(event.server.id)
+
     logger.debug "\e[1;32mLeft voice channel #{event.user.voice_channel.name.inspect}\e[0m"
   end
 
   def flush_message_buffer!
     buffer = event.drain_into('')
-    logger.debug "Flushed message buffer: #{buffer.inspect}"
-    event.send_message(buffer) unless buffer.empty?
-    nil
+    if buffer.present?
+      event.send_message(buffer) 
+      logger.debug "Flushed message buffer: #{buffer.inspect}"
+    end
+    return nil
   end
 
   def server_mutex
